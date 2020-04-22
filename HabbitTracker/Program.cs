39e1a0baa3b9c370ffe9c4e1deeba9396fd834b6
Saso Dimitrovski <sasodimitrovski1982@yahoo.com>
2020-04-22 20:14:37 +0200

    first init

---
+++ HabbitTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HabbitTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
